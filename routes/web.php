4878ae22825af2d1448c8d55138537976c4e3c82
IPKIN9 <irwandirini@gmail.com>
2023-06-21 10:51:23 +0800

    Add route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ Route::get('/kelas', function () {
     return view('page.Kelas');
 });
 
+Route::get('/mapel', function () {
+    return view('page.Mapel');
+});
+
 Route::get('/jurusan', function () {
     return view('page.Jurusan');
+});
+
+Route::get('/pangkat', function () {
+    return view('page.Pangkat');
 });
